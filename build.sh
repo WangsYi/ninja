3b05daa43afa4b4c77e35aafa503ad92293e24eb
wangs <wangs13@foxmail.com>
2024-04-01 00:16:55 +0800

    modify build.sh

--- build.sh
+++ build.sh
@@ -19,17 +19,17 @@ target_architectures=(
     "armv5te-unknown-linux-musleabi"
     "i686-unknown-linux-gnu"
     "i586-unknown-linux-gnu"
-    "x86_64-pc-windows-msvc"
+    # "x86_64-pc-windows-msvc"
 )
 
 pull_docker_image() {
-    image="ghcr.io/gngpp/rust-musl-cross:$1"
+    image="messense/rust-musl-cross:$1"
     echo "Pulling $image"
     docker pull $image
 }
 
 rmi_docker_image() {
-    image="ghcr.io/gngpp/rust-musl-cross:$1"
+    image="messense/rust-musl-cross:$1"
     echo "Removing $image docker image"
     if [ "$rmi" = "true" ]; then
         docker rmi $image
@@ -49,7 +49,7 @@ build_macos_target() {
 }
 
 build_linux_target() {
-    docker_image="ghcr.io/gngpp/rust-musl-cross:$1"
+    docker_image="messense/rust-musl-cross:$1"
 
     features=""
     if [ "$1" = "armv5te-unknown-linux-musleabi" ] || [ "$1" = "arm-unknown-linux-musleabi" ] || [ "$1" = "arm-unknown-linux-musleabihf" ]; then
@@ -84,7 +84,7 @@ build_linux_target() {
 }
 
 build_windows_target() {
-    docker_image="ghcr.io/gngpp/rust-musl-cross:$1"
+    docker_image="messense/rust-musl-cross:$1"
 
     echo "Building $1"
     docker run --rm -t \
