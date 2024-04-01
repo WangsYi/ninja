005f368defe10c3d826ccfc89e2d2d849cb082ff
wangsyi <wangs13@foxmail.com>
2024-04-01 23:10:50 +0800

    update dockerfile

--- docker/build.sh
+++ docker/build.sh
@@ -3,7 +3,13 @@
 : ${tag=latest}
 
 cd docker
-docker buildx build --platform linux/amd64,linux/arm64/v8,linux/arm/v7,linux/arm/v6 \
+# docker buildx build --platform linux/amd64,linux/arm64/v8,linux/arm/v7,linux/arm/v6 \
+#     --tag ghcr.io/wangsyi/ninja:$tag \
+#     --tag ghcr.io/wangsyi/ninja:latest \
+#     --tag wangsyi/ninja:$tag \
+#     --tag wangsyi/ninja:latest \
+#     --build-arg VERSION=$tag --push .
+docker buildx build --platform linux/amd64,linux/arm/v7,linux/arm/v6 \
     --tag ghcr.io/wangsyi/ninja:$tag \
     --tag ghcr.io/wangsyi/ninja:latest \
     --tag wangsyi/ninja:$tag \
