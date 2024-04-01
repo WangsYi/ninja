2966db091ac63a63e7831f7838bf23eb45f8cc8f
wangsyi <wangs13@foxmail.com>
2024-04-01 21:10:45 +0800

    mod build image

--- build.sh
+++ build.sh
@@ -23,13 +23,13 @@ target_architectures=(
 )
 
 pull_docker_image() {
-    image="wangsyi/ninja-builder:$1"
+    image="ghcr.io/wangsyi/ninja-builder:$1"
     echo "Pulling $image"
     docker pull $image
 }
 
 rmi_docker_image() {
-    image="wangsyi/ninja-builder:$1"
+    image="ghcr.io/wangsyi/ninja-builder:$1"
     echo "Removing $image docker image"
     if [ "$rmi" = "true" ]; then
         docker rmi $image
@@ -49,7 +49,7 @@ build_macos_target() {
 }
 
 build_linux_target() {
-    docker_image="wangsyi/ninja-builder:$1"
+    docker_image="ghcr.io/wangsyi/ninja-builder:$1"
 
     features=""
     if [ "$1" = "armv5te-unknown-linux-musleabi" ] || [ "$1" = "arm-unknown-linux-musleabi" ] || [ "$1" = "arm-unknown-linux-musleabihf" ]; then
@@ -84,7 +84,7 @@ build_linux_target() {
 }
 
 build_windows_target() {
-    docker_image="wangsyi/ninja-builder:$1"
+    docker_image="ghcr.io/wangsyi/ninja-builder:$1"
 
     echo "Building $1"
     docker run --rm -t \
